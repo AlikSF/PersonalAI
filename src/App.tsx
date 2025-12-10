fc556e26eb85b3f5fc5e8bc14a8360a168bec45e
Aalambek Personal <78116253+AlikSF@users.noreply.github.com>
2025-12-10 10:22:49 +0100

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -258,12 +258,12 @@ function AppContent() {
               </div>
             ) : (
               <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
-                {filteredProducts.map((product, index) => (
+                {filteredProducts.map((product) => (
                   <ProductCard
                     key={product.id}
                     product={product}
                     onViewDetails={handleViewDetails}
-                    eager={index < 6}
+                    eager={true}
                   />
                 ))}
               </div>
